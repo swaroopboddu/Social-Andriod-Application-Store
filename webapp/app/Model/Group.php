bd779d3b2797c387bf69b3fc2df831ac3896921c
Avinash Vallabhaneni <avinash.vllbh@gmail.com>
2014-04-12 18:53:57 -0700

    Added Group belongsTo model association

--- webapp/app/Model/Group.php
+++ webapp/app/Model/Group.php
@@ -28,7 +28,12 @@ class Group extends AppModel {
 		),
 	);
 
-	//public $hasOne = 'User';
+	public $belongsTo = 'User';
+	// public $hasOne = array(
+	// 	'User' => array(
+	// 		'className' => 'User',
+	// 		''
+	// 		);
 
 	// var $hasAndBelongsToMany = array(
 	// 'User' => array(
